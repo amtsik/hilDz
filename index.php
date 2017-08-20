f2f1dd2df17a1199aaabcf4f30c6f135e7e0731b
Artem <amt@em>
2017-08-20 15:01:57 +0300

    termometr

--- index.php
+++ index.php
@@ -32,6 +32,14 @@ function getTextTemp($a)
     }
 }
 
+function getFar($a) {
+    return     ( 9 / 5) * $a + 32;
+}
+
+function getCel($a) {
+    return     ( 5 / 9) * ($a - 32);
+}
+
 $a2 = explode('&', $_SERVER['QUERY_STRING']);
 
 foreach($a2 as $str) {
@@ -45,11 +53,16 @@ if ( $a1['degree'] === null) {
     exit('Значение температуры не указано');
 }
 
+
 if ($a1['type'] == 'f') {
-    $a1['text'] = getTextTemp(( 5 / 9) * ($a1['degree'] -32));
+    $a1['degreeF'] = $a1['degree'];
+    $a1['degreeC'] = getCel($a1['degree']);
+    $a1['text'] = getTextTemp( $a1['degreeC'] );
 } else {
-    $a1['text'] = getTextTemp($a1['degree']);
+    $a1['degreeC'] = $a1['degree'];
+    $a1['degreeF'] = getFar($a1['degree']);
+    $a1['text'] = getTextTemp( $a1['degreeC'] );
     $a1['type'] == 'c';
 }
 
-echo 'На улице '.$a1['text'].'. температура = '.$a1['degree'].' по '.$a1['type'].'.';
+echo 'На улице '.$a1['text'].'. температура = '.$a1['degreeC'].' по цельсию или '.$a1['degreeF'].' по Фарингейту ';
